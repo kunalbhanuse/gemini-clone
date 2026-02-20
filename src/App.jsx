8fb9cfc93e9597d5cefdb9893ce269173a993691
kunal bhanuse <kunalbhanuse07@gmail.com>
2026-02-20 23:14:23 +0530

    background change

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 style={{ color: "brown" }}>Rushi Kale Hacker</h1>
-    </>
+    <div
+      style={{
+        height: "100vh", // full viewport height
+        width: "100%", // full width
+        backgroundImage: `url("https://images.pexels.com/photos/672630/pexels-photo-672630.jpeg?_gl=1*rd18z*_ga*MTQyMzgzMzI1NS4xNzUxNDM1MDI1*_ga_8JE65Q40S6*czE3NzE2MDgyOTckbzEwJGcxJHQxNzcxNjA4ODIwJGo1NyRsMCRoMA..")`,
+        backgroundSize: "cover", // cover whole div
+
+        color: "purple",
+        fontSize: "2rem",
+      }}
+    >
+      <h1 className="rushi" style={{}}>
+        Rushi Kale Hacker
+      </h1>
+    </div>
   );
 }
 
